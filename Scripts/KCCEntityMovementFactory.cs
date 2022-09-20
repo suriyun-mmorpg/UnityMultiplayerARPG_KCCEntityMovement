34e491cad4d2ae81e5d4a926c6dfef889e0711e7
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-09-20 09:04:08 +0700

    Add factory class

---
+++ Scripts/KCCEntityMovementFactory.cs
@@ -0,0 +1,58 @@
+using KinematicCharacterController;
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public class KCCEntityMovementFactory : IEntityMovementFactory
+    {
+        public string Name => "KCC Entity Movement Factory";
+
+        public DimensionType DimensionType => DimensionType.Dimension3D;
+
+        public KCCEntityMovementFactory()
+        {
+
+        }
+
+        public bool ValidateSourceObject(GameObject obj)
+        {
+            return true;
+        }
+
+        public IEntityMovementComponent Setup(GameObject obj, ref Bounds bounds)
+        {
+            bounds = default;
+            MeshRenderer[] meshes = obj.GetComponentsInChildren<MeshRenderer>();
+            for (int i = 0; i < meshes.Length; ++i)
+            {
+                if (i > 0)
+                    bounds.Encapsulate(meshes[i].bounds);
+                else
+                    bounds = meshes[i].bounds;
+            }
+
+            SkinnedMeshRenderer[] skinnedMeshes = obj.GetComponentsInChildren<SkinnedMeshRenderer>();
+            for (int i = 0; i < skinnedMeshes.Length; ++i)
+            {
+                if (i > 0)
+                    bounds.Encapsulate(skinnedMeshes[i].bounds);
+                else
+                    bounds = skinnedMeshes[i].bounds;
+            }
+
+            float scale = Mathf.Max(obj.transform.localScale.x, obj.transform.localScale.y, obj.transform.localScale.z);
+            bounds.size = bounds.size / scale;
+            bounds.center = bounds.center / scale;
+
+            obj.AddComponent<Rigidbody>();
+            obj.AddComponent<CapsuleCollider>();
+
+            KinematicCharacterMotor kccMotor = obj.AddComponent<KinematicCharacterMotor>();
+            float height = bounds.size.y;
+            float radius = Mathf.Min(bounds.extents.x, bounds.extents.z);
+            kccMotor.SetCapsuleDimensions(radius, height, height * 0.5f);
+
+            return obj.AddComponent<RigidBodyEntityMovement>();
+        }
+    }
+}
