48a71297b0983978d701a09d244c090f3d0f321f
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-12-02 07:59:24 +0700

    feat: change name and condition from `useRootMotionWhileNotMoving` to `alwaysUseRootMotion`

--- Scripts/KCCEntityMovement.cs
+++ Scripts/KCCEntityMovement.cs
@@ -4,6 +4,7 @@ using LiteNetLibManager;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Serialization;
 
 namespace MultiplayerARPG
 {
@@ -40,11 +41,12 @@ namespace MultiplayerARPG
         public float forceUngroundAfterJumpDuration = 0.1f;
 
         [Header("Root Motion Settings")]
+        [FormerlySerializedAs("useRootMotionWhileNotMoving")]
+        public bool alwaysUseRootMotion;
         public bool useRootMotionForMovement;
         public bool useRootMotionForAirMovement;
         public bool useRootMotionForJump;
         public bool useRootMotionForFall;
-        public bool useRootMotionWhileNotMoving;
         public bool useRootMotionUnderWater;
 
         [Header("Networking Settings")]
@@ -108,11 +110,11 @@ namespace MultiplayerARPG
                 afterCrawlingPauseMovementDuration = afterCrawlingPauseMovementDuration,
                 underWaterThreshold = underWaterThreshold,
                 autoSwimToSurface = autoSwimToSurface,
+                alwaysUseRootMotion = alwaysUseRootMotion,
                 useRootMotionForMovement = useRootMotionForMovement,
                 useRootMotionForAirMovement = useRootMotionForAirMovement,
                 useRootMotionForJump = useRootMotionForJump,
                 useRootMotionForFall = useRootMotionForFall,
-                useRootMotionWhileNotMoving = useRootMotionWhileNotMoving,
                 useRootMotionUnderWater = useRootMotionUnderWater,
                 snapThreshold = snapThreshold,
             };
